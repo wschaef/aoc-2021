7ce5227222281057683658f18ff78ee633ab7520
schaefer.w@telekom.de <schaefer.w@telekom.de>
2021-12-02 12:27:48 +0100

    feat:add lodash

--- day00/src/solution1.ts
+++ day00/src/solution1.ts
@@ -1,8 +1,8 @@
+import * as _ from "lodash";
 import { Solver } from "./solver"
 
 const solution: any = (input: string) => {
-    const entries = input.split("\n")
-    return entries
+    const entries = _.chain(input.split("\n"))
+    return entries.value()
 }
-
 new Solver(solution, 'input.txt', 1).print()
