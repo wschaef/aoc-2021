2b19533b31518b08ed325224c61710a1b3dee28d
schaefer.w@telekom.de <schaefer.w@telekom.de>
2021-12-16 08:22:40 +0100

    feat: day 15 part 1 and 2

--- day15/src/solution1.ts
+++ day15/src/solution1.ts
@@ -1,76 +1,63 @@
-import * as _ from "lodash";
-import path from "path/posix";
 import { Solver } from "./solver"
 
-class Point {
-    public neighbours: Point[] = new Array<Point>()
-    constructor(public risk: number, public i: number, public j: number) { }
+class Node {
+    public neighbours: Node[] = new Array<Node>()
+    public distance: number = Number.MAX_VALUE
+    public visited: boolean = false
+    constructor(public value: number, public i: number, public j: number) { }
 }
 
-class Path {
-    public risk = 0
-    public points: Set<Point>
-    public last: Point
-    constructor(private start: Point) {
-        this.points = new Set([start])
-        this.risk = start.risk
-        this.last = start
-    }
-    public add(point: Point): Path {
-        this.points.add(point)
-        this.risk += point.risk
-        this.last = point
-        return this
-    }
-    public copy(): Path {
-        const newPath = new Path(this.start)
-        newPath.risk = this.risk
-        newPath.points = new Set(this.points)
-        newPath.last = this.last
-        return newPath
-    }
-    public hasPoint(point: Point): boolean {
-        return this.points.has(point)
-    }
-    public toString() {
-        const points = Array.from(this.points.values()).map(p => p.risk)
-        return `${this.risk} -- ${this.id()}`
-    }
-    public id(): string {
-        return Array.from(this.points.values()).map(p => p.risk).join(',')
-    }
-}
 
-class State {
-    constructor(public minRisk: number) { }
-    public update(risk: number) {
-        this.minRisk = Math.min(this.minRisk, risk)
-    }
-}
-
-const solution: any = (input: string) => {
+const solution = (input: string) => {
     const lines = input.split("\n").map(line => line.split('').map(it => parseInt(it)))
-    const map = lines.map((line, i) => line.map((field, j) => new Point(field, i, j))) // used for lookup the points
-    const points = map.flat();
-    points.forEach(p => p.neighbours = getNeighbours(lines, p.i, p.j, map))
+    const map = lines.map((line, i) => line.map((field, j) => new Node(field, i, j))) // used for lookup the points
+    const nodes = map.flat();
+    nodes.forEach(n => n.neighbours = getNeighbours(lines, n.i, n.j, map))
+    // input is parsed to an array of nodes with neighbours as references
 
-    const start = points[0];
-    const path = new Path(start);
+    const unvisited = new Set(nodes)
+    const startNode = nodes[0]
+    startNode.distance = 0
+    const endNode = nodes.slice(-1)[0]
 
-    let paths = new Map([[path.id(), path]])
-    const state = new State(Number.MAX_VALUE)
-    step(path, paths, points.slice(-1)[0], state)
-    print(paths);
-    console.log(state)
-    return state.minRisk - start.risk
+
+    let currentNode = startNode
+    do {
+        currentNode = iterate(currentNode, endNode, unvisited)
+    } while (currentNode != endNode);
+
+    console.log(currentNode.distance)
+
+    // console.table(unvisited)
+    return currentNode.distance
 }
 
-function print(paths: Map<string, Path>) {
-    console.table(Array.from(paths.values()).map(it => it.toString()));
+function iterate(startNode: Node, endNode: Node, unvisited: Set<Node>): Node {
+
+    let currentNode = startNode
+    // console.log('currentNode: ', currentNode.i, currentNode.j, currentNode.distance);
+
+    currentNode.neighbours.filter(n => !n.visited)
+        .forEach(n => n.distance = Math.min(n.distance, currentNode.distance + n.value))
+    currentNode.visited == true
+    unvisited.delete(currentNode)
+
+    if (currentNode == endNode) {
+        console.log("ende")
+        return currentNode
+    }
+
+    let min = Number.MAX_VALUE
+    unvisited.forEach(n => {
+        if (min >= n.distance) {
+            min = n.distance
+            currentNode = n
+        }
+    })
+    return currentNode
 }
 
-// return 3,5 or 8 neighbour points
-function getNeighbours(lines: number[][], i: number, j: number, dict: Point[][]): Point[] {
+function getNeighbours(lines: number[][], i: number, j: number, dict: Node[][]): Node[] {
     const coordinates = [[i - 1, j], [i, j + 1], [i + 1, j], [i, j - 1]]
         // const coordinates = [[i, j + 1], [i + 1, j]]
         .filter(([I, J]) => I >= 0 && I < lines.length && J >= 0 && J < lines[0].length)
@@ -78,44 +65,4 @@ function getNeighbours(lines: number[][], i: number, j: number, dict: Point[][])
     return result
 }
 
-function step(path: Path, paths: Map<string, Path>, end: Point, state: State) {
-    if (path.points.size > 1000) {
-        return
-    }
-    let deadlock = true // if all neigbours are part of path
-    path.last.neighbours.forEach(p => {
-        if (!path.hasPoint(p)) {
-            deadlock = false
-            // console.log(p.risk, path.toString())
-            paths.delete(path.id())
-            const newPath = path.copy().add(p)
-            if (newPath.risk < state.minRisk) {
-                paths.set(newPath.id(), newPath)
-            }
-            //print(paths) 
-            if (p !== end) {
-                if (newPath.risk < state.minRisk) {
-                    step(newPath, paths, end, state)
-                }
-            } else {
-                state.update(newPath.risk)
-                //cleanUp(paths, state)
-                console.log(paths.size, state)
-            }
-        }
-    })
-    if (deadlock) {
-        paths.delete(path.id())
-    }
-    // console.log(state, paths.size)
-}
-
-function cleanUp(paths: Map<string, Path>, state: State) {
-    for (const path of paths.entries()) {
-        if (path[1].risk > state.minRisk) {
-            paths.delete(path[0])
-        }
-    }
-}
-
-new Solver(solution, 'input.txt', 1).print()
+new Solver(solution, 'input1.txt', 1).print()
--- day15/src/solution2.ts
+++ day15/src/solution2.ts
@@ -1,8 +1,93 @@
 import { Solver } from "./solver"
 
-const solution = (input: string) => {
-    const entries = input.split(",")
-    return entries
+class Node {
+    public neighbours: Node[] = new Array<Node>()
+    public distance: number = Number.MAX_VALUE
+    public visited: boolean = false
+    constructor(public value: number, public i: number, public j: number) { }
 }
 
-//new Solver(solution, 'input.txt', 2).print()
+
+const solution = (input: string) => {
+    let lines = input.split("\n").map(line => line.split('').map(it => parseInt(it)))
+    //make the field 5x bigger to right
+    lines = lines.map(line => [
+        ...line,
+        ...line.map(n => increase(n, 1)),
+        ...line.map(n => increase(n, 2)),
+        ...line.map(n => increase(n, 3)),
+        ...line.map(n => increase(n, 4)),
+    ])
+    // make the field 5x bigger down
+    lines = [
+        ...lines,
+        ...lines.map(line => line.map(n => increase(n, 1))),
+        ...lines.map(line => line.map(n => increase(n, 2))),
+        ...lines.map(line => line.map(n => increase(n, 3))),
+        ...lines.map(line => line.map(n => increase(n, 4))),
+    ]
+    const map = lines.map((line, i) => line.map((field, j) => new Node(field, i, j))) // used for lookup the points
+    const nodes = map.flat();
+    nodes.forEach(n => n.neighbours = getNeighbours(lines, n.i, n.j, map))
+    // input is parsed to an array of nodes with neighbours as references
+
+    const unvisited = new Set(nodes)
+    const startNode = nodes[0]
+    startNode.distance = 0
+    const endNode = nodes.slice(-1)[0]
+
+
+    let currentNode = startNode
+    do {
+        currentNode = iterate(currentNode, endNode, unvisited)
+    } while (currentNode != endNode);
+
+    console.log(currentNode.distance)
+
+    // console.table(unvisited)
+    return currentNode.distance
+}
+
+function iterate(startNode: Node, endNode: Node, unvisited: Set<Node>): Node {
+
+    let currentNode = startNode
+    // console.log('currentNode: ', currentNode.i, currentNode.j, currentNode.distance);
+
+    currentNode.neighbours.filter(n => !n.visited)
+        .forEach(n => n.distance = Math.min(n.distance, currentNode.distance + n.value))
+    currentNode.visited == true
+    unvisited.delete(currentNode)
+
+    if (currentNode == endNode) {
+        console.log("ende")
+        return currentNode
+    }
+
+    let min = Number.MAX_VALUE
+    unvisited.forEach(n => {
+        if (min >= n.distance) {
+            min = n.distance
+            currentNode = n
+        }
+    })
+    return currentNode
+
+}
+
+function increase(n: number, by: number): number {
+    let result = n + by
+    if (result > 9) {
+        result = result - 9
+    }
+    return result
+}
+
+function getNeighbours(lines: number[][], i: number, j: number, dict: Node[][]): Node[] {
+    const coordinates = [[i - 1, j], [i, j + 1], [i + 1, j], [i, j - 1]]
+        // const coordinates = [[i, j + 1], [i + 1, j]]
+        .filter(([I, J]) => I >= 0 && I < lines.length && J >= 0 && J < lines[0].length)
+    const result = coordinates.map(([I, J]) => dict[I][J])
+    return result
+}
+
+new Solver(solution, 'input1.txt', 2).print()
